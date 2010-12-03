9bf7312234c8bf20bc8a077e8781d3df1ce45ef9
Dario Javier Cravero (atlas) <dario@starlight.ie>
2010-12-03 21:31:35 +0000

    - Added a helper to clean up the controller

--- app/controllers/images.rb
+++ app/controllers/images.rb
@@ -2,24 +2,13 @@ Cuva.controllers :images do
     set :haml, :format => :html5
 
     get :index, :map => "/" do
+        # Init the session and destroy wathever is there.
+        init_session(true)
 
         render "images/index"
     end
 
-    post :upload, :map => '/upload' do
-        if !session.key?(:path)
-            session[:path] = "public/images/" + Time.now.to_i.to_s + "/"
-            begin
-                Dir.mkdir(session[:path])
-            rescue
-            end
-        end
-
-        fname = session[:path] + env['HTTP_X_FILE_NAME']
-        tempfile = File.new(fname, "wb")
-        tempfile << env["rack.input"].read
-        tempfile.close
-
-        JSON.generate({:image => fname.gsub("public", "")})
+    post :upload, :map => "/upload", :provides => :json do
+        {:image => save_temporary_image}.to_json
     end
 end
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,7 +1,23 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Cuva.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+    def init_session(destroy=false)
+        if destroy && !session.key?(:path)
+            session[:path] = "public/images/" + Time.now.to_i.to_s + "/"
+            begin
+                Dir.mkdir(session[:path])
+            rescue
+            end
+        end
+    end
+
+    def save_temporary_image
+        file_name = session[:path] + env['HTTP_X_FILE_NAME']
+
+        temp_file = File.new(file_name, "wb")
+        temp_file << env["rack.input"].read
+        temp_file.close
+
+        file_name.gsub("public", "")
+    end
 end
